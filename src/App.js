2e9e335e52caa4e184997adfcb56ac691093ee40
markinascimento <markinhonascimento50@gmail.com>
2021-07-12 22:41:14 -0300

    finished the front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Header } from './components/Header/Header';
+import { Catalog } from './components/Catalog/Catalog';
+import { Footer } from './components/Footer/Footer';
+
+import bannerImg from './assets/banner.svg';
+import arrowLeft from './assets/arrow-left.svg';
+import arrowRigth from './assets/arrow-rigth.svg';
+
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+        <Header/>
+
+
+      {/* Banner */}
+      <img src={bannerImg} alt="bannerImg" />
+
+
+      {/* Title Best Sellers */}
+      <div className="container-shelf">
+        <span> Mais vendidos </span>
+        <div className="details-bar"></div>
+      </div>
+
+
+      {/* Container Catalog */}
+      <div className="container-catalog">
+        <img src={arrowLeft} alt="arrowLeft" className="arrowLeft"/>
+        <Catalog/>
+        <Catalog/>
+        <Catalog/>
+        <Catalog/>
+        <img src={arrowRigth} alt="arrowRigth" className="arrowRigth"/>
+      </div>
+
+
+      {/* Container Event */}
+      <div className="container-event">
+        <h1> Participe de nossas news com promoções e novidades! </h1>
+
+        <form className="container-form">
+          <input type="text" className="input"/>
+
+          <input type="text" className="input"/>
+
+          <button className="button-submit">
+            <p> Eu quero! </p>
+          </button>
+        </form>
+      </div>
+
+      <div className="contianer-footer">
+        <Footer/>
+      </div>
+
+
     </div>
   );
 }
